26e85b4b681c1bf43b483e319bb332f9754cca39
Chi-Chidev <fabulousesther05@gmail.com>
2025-04-18 22:56:07 +0100

    fifth commit

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const iconOne = questionOne.querySelector("img");
 questionOne.addEventListener("click", function () {
   if (answerOne.style.display === "block") {
     answerOne.style.display = "none"; // Hide answer
-    iconOne.src = "assets/images/icon-plus.svg"; // Change to plus icon
+    iconOne.src = "images/icon-plus.svg"; // Change to plus icon
     iconOne.alt = "plus icon";
   } else {
     answerOne.style.display = "block"; // Show answer
-    iconOne.src = "assets/images/icon-minus.svg"; // Change to minus icon
+    iconOne.src = "images/icon-minus.svg"; // Change to minus icon
     iconOne.alt = "minus icon";
   }
 });
@@ -22,11 +22,11 @@ const iconTwo = questionTwo.querySelector("img"); // Get the image inside q1
 questionTwo.addEventListener("click", function () {
   if (answerTwo.style.display === "none") {
     answerTwo.style.display = "block"; // Show answer
-    iconTwo.src = "assets/images/icon-minus.svg"; // Change to minus icon
+    iconTwo.src = "images/icon-minus.svg"; // Change to minus icon
     iconTwo.alt = "minus icon";
   } else {
     answerTwo.style.display = "none"; // Hide answer
-    iconTwo.src = "assets/images/icon-plus.svg"; // Change to plus icon
+    iconTwo.src = "images/icon-plus.svg"; // Change to plus icon
     iconTwo.alt = "plus icon";
   }
 });
@@ -38,11 +38,11 @@ const iconThree = questionThree.querySelector("img");
 questionThree.addEventListener("click", function () {
   if (answerThree.style.display === "none") {
     answerThree.style.display = "block";
-    iconThree.src = "assets/images/icon-minus.svg";
+    iconThree.src = "images/icon-minus.svg";
     iconThree.alt = "minus icon";
   } else {
     answerThree.style.display = "none";
-    iconThree.src = "assets/images/icon-plus.svg";
+    iconThree.src = "images/icon-plus.svg";
     iconThree.alt = "plus icon";
   }
 });
@@ -54,11 +54,11 @@ const iconFour = questionFour.querySelector("img");
 questionFour.addEventListener("click", function () {
   if (answerFour.style.display === "none") {
     answerFour.style.display = "block";
-    iconFour.src = "assets/images/icon-minus.svg";
+    iconFour.src = "images/icon-minus.svg";
     iconFour.alt = "minus icon";
   } else {
     answerFour.style.display = "none";
-    iconFour.src = "assets/images/icon-plus.svg";
+    iconFour.src = "images/icon-plus.svg";
     iconFour.alt = "plus icon";
   }
 });
